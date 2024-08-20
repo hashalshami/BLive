755426a7c6c73ce9047fc79a79276f1b2da8cce3
hashemSoft <hashalshamii@gmail.com>
2024-08-21 02:07:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const bar = document.getElementById('bar');
+const closeBtn = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active')
+    })
+}
+
+if (closeBtn) {
+    closeBtn.addEventListener('click', () =>{
+        nav.classList.remove('active')
+    })
+}
